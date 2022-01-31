967b5bfc725279e6bcae51b593b6930b330d9b3e
Brian <bstanton773@gmail.com>
2022-01-31 16:38:06 -0600

    Passing function props, componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import Button from './components/Button';
 import Navbar from './components/Navbar';
+import RacerList from './components/RacerList';
 
 export default class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    incrementTotalCount = (step) => {
+        console.log('The button has been clicked, friend')
+        console.log(step)
+        const newCount = this.state.count + step
+        this.setState({
+            count: newCount
+        })
+    }
 
     render() {
         const name = 'Brian'
@@ -11,10 +27,12 @@ export default class App extends Component {
                 <Navbar />
                 <div className='container'>
                     <h1>Hello {name}</h1>
-                    <Button step={1}/>
-                    <Button step={5}/>
-                    <Button step={10}/>
-                    <Button step={100}/>
+                    <h4>{this.state.count}</h4>
+                    <Button step={1} handleClick={this.incrementTotalCount}/>
+                    <Button step={5} handleClick={this.incrementTotalCount}/>
+                    <Button step={10} handleClick={this.incrementTotalCount}/>
+                    <Button step={100} handleClick={this.incrementTotalCount}/>
+                    <RacerList />
                 </div>
             </>
         );
