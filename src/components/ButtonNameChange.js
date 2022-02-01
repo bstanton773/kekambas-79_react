21f3bd5c42b19a7db3247c2086e96f1bb6f0e2e6
Brian <bstanton773@gmail.com>
2022-02-01 14:16:05 -0600

    Add useState to ButtonNameChange

--- src/components/ButtonNameChange.js
+++ src/components/ButtonNameChange.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function ButtonNameChange(props){
-    
+    const [buttonColor, setButtonColor] = useState('secondary')
     const handleForm = (e) => {
         e.preventDefault();
         const newName = e.target.newName.value;
@@ -9,12 +9,15 @@ export default function ButtonNameChange(props){
     }
 
     return (
+        <>
         <form onSubmit={handleForm}>
             <div className="mb-3">
                 <label htmlFor="newName" className="form-label">New Name</label>
                 <input type="text" name="newName" className="form-control" />
             </div>
-            <input type="submit" className="btn btn-secondary" />
+            <input type="submit" className={`btn btn-${buttonColor}`} />
         </form>
+        <button className="btn btn-primary" onClick={() => {setButtonColor(buttonColor == 'secondary' ? 'warning' : 'secondary')}}>Change Above Button Color</button>
+        </>
     )
 }
